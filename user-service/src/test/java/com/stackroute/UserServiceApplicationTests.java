17ce03112f9aa30c7ae17ae1f33d203aa1083364
Manu S <manu.s@globallogic.com>
2022-07-12 21:02:45 +0530

    user services apis

--- user-service/src/test/java/com/stackroute/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/stackroute/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.stackroute;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
 
 @SpringBootTest
+@TestExecutionListeners
 class UserServiceApplicationTests {
 
 	@Test
