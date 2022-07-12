01c1caa39ea7cf628361ceae4e468541a698128f
Manu S <manu.s@globallogic.com>
2022-07-12 20:15:33 +0530

    apis for user service

--- user-service/src/main/java/com/stackroute/model/Patient.java
+++ user-service/src/main/java/com/stackroute/model/Patient.java
@@ -1,7 +1,6 @@
 package com.stackroute.model;
 
 import lombok.*;
-import org.bson.types.Binary;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
