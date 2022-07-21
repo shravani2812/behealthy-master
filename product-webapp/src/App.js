ac08a0fee6f88f8143f2abfc1f775dfb458a73ca
naveenkumar.v <naveenkumar.v@globallogic.com>
2022-07-21 14:19:45 +0530

    routing config

--- product-webapp/src/App.js
+++ product-webapp/src/App.js
@@ -1,11 +1,57 @@
-
+import React from 'react';
 import './App.css';
-import UserHome from './userlanding/UserHome';
+
+//import PatientLanding from './components/patient-profile/pateintlanding/PatientLanding';
+import UserHome from './components/userlanding/UserHome';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PatientLanding from './components/patient-profile/pateintlanding/PatientLanding';
+import HomePage from './components/common/HomePage';
+import Main from './components/doctor-profile/Main';
+//import SideNavBar from './components/common/Sidebar';
+import Pateintroute from './components/patient-profile/components/Pateintroute';
+import Profile from './components/patient-profile/pages/Profile'
+import Support from './components/patient-profile/pages/Support';
+import Appointments from './components/patient-profile/pages/Appointments';
+//import Login from './components/login/Login';
+import Login2 from './components/login/Login2';
+import Register from './components/register/Register';
+
+
+
 function App() {
   return (
-    <div >
+    <>
      
-    </div>
+      {/* <Pateintroute/>   */}
+    {/* <UserHome/>  */}
+    {/* <SideNavBar/> */}
+     {/* <Main/>    */}
+      {/* <HomePage/> */}
+     {/* <PatientLanding/> */}
+     <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<UserHome />} /> 
+        <Route path="/patient" element={<PatientLanding />} />
+        <Route path="doctor" element={<HomePage/>} >
+           <Route path="profile" element={<Main/>}/>
+        </Route>
+       
+       
+     
+
+       <Route path="patientpro" element={<Pateintroute/>}>
+       <Route path="appointment" element={<Appointments />} />
+        <Route path="support" element={<Support />} />
+        <Route path="profile" element={<Profile />} />
+      </Route > 
+
+      <Route path='/login' element={<Login2/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
+
+      </Routes>
+    </BrowserRouter>
+    </>
+    
   );
 }
 
